6fa953019b2141ebed1dd54dd018c6d50736c279
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-18 12:30:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,8 @@ resource "aws_security_group" "wordpress_sg" {
   }
  ingress {
     from_port   = 8080
-    to_port     = 8080  protocol    = "tcp"
+    to_port     = 8080  
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   # HTTPS access from anywhere
