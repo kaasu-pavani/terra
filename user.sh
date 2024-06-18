07cd18699217d2704373fa05036bd358c33c0504
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-18 12:34:01 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo yum -y install git docker
+sudo yum install -y git
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
-sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 git clone https://github.com/kaasu-pavani/wordpress.git
 cd wordpress/
 docker-compose up -d
